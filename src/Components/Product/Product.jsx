5fe671374da8078457fb00060453a84899456561
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-26 02:05:13 +0530

    product

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -3,65 +3,33 @@ import axios from "axios";
 
 const Product = () => {
   const [data, setData] = useState([]);
-  // console.log(data);
 
   const getData = async () => {
-    try{
+    try {
       axios
-      .get("https://myindiaa-deployement.onrender.com/products")
-      .then((res) =>{
-        console.log(res.data)
-        setData(res.data);
-      });
-    }catch(err){
+        .get("https://myindiaa-deployement.onrender.com/products")
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        });
+    } catch (err) {
       console.log(err);
     }
   };
 
-
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[])
- 
+  }, []);
 
   return (
     <div className="container mx-auto px-6">
-      <div
-        className="h-64 rounded-md overflow-hidden bg-cover bg-center"
-        // style="background-image: url('https://images.unsplash.com/photo-1577655197620-704858b270ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=144')"
-      >
-        <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
-          <div className="px-10 max-w-xl">
-            <h2 className="text-2xl text-white font-semibold">Sport Shoes</h2>
-            <p className="mt-2 text-gray-400">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
-              facere provident molestias ipsam sint voluptatum pariatur.
-            </p>
-            <button className="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-              <span>Shop Now</span>
-              <svg
-                className="h-5 w-5 mx-2"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
       <div className="md:flex mt-8 md:-mx-4">
-        <div
-          className="w-full h-64 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:w-1/2"
-          // style="background-image: url('https://images.unsplash.com/photo-1547949003-9792a18a2601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')"
-        >
+        <div className="w-full h-64 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:w-1/2 bg-man-products">
           <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
             <div className="px-10 max-w-xl">
-              <h2 className="text-2xl text-white font-semibold">Back Pack</h2>
+              <h2 className="text-2xl text-white font-semibold">
+                Men's Products
+              </h2>
               <p className="mt-2 text-gray-400">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Tempore facere provident molestias ipsam sint voluptatum
@@ -74,7 +42,7 @@ const Product = () => {
                   fill="none"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  stroke-width="2"
+                  strokeWidth="2"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
@@ -84,13 +52,12 @@ const Product = () => {
             </div>
           </div>
         </div>
-        <div
-          className="w-full h-64 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md:w-1/2"
-          // style="background-image: url('https://images.unsplash.com/photo-1486401899868-0e435ed85128?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')"
-        >
+        <div className="w-full h-64 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md:w-1/2 bg-women-products">
           <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
             <div className="px-10 max-w-xl">
-              <h2 className="text-2xl text-white font-semibold">Games</h2>
+              <h2 className="text-2xl text-white font-semibold">
+                Women's Products
+              </h2>
               <p className="mt-2 text-gray-400">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Tempore facere provident molestias ipsam sint voluptatum
@@ -103,7 +70,7 @@ const Product = () => {
                   fill="none"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  stroke-width="2"
+                  strokeWidth="2"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
@@ -114,39 +81,24 @@ const Product = () => {
           </div>
         </div>
       </div>
-
-{/* product page */}
-
-
-
 
       <div className="mt-16">
-        <h3 className="text-gray-600 text-2xl font-medium">Fashions</h3>
-
+        <h3 className="text-gray-600 text-2xl font-medium">Products</h3>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-
-          {
-              data.map((item)=>(
-                <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-           
-                <div
-                  className="flex items-end justify-end h-56 w-full bg-cover">
-                  <img
-                    src={item.image}
-                    alt="pic"
-                  />
-                </div>
-                <div className="px-5 py-3">
-                  <h3 className="text-gray-700 uppercase">{item.title}</h3>
-                  <span className="text-gray-500 mt-2">{item.price} Rs</span>
-                </div>
-                <button class="w-1/2 p-2 rounded bg-blue-600 text-white mx-20 mb-4 hover:bg-green-500 focus:outline-none focus:bg-blue-500">
-                        See Details
-                    </button>
-
+          {data.map((item) => (
+            <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden" key={item.id}>
+              <div className="flex items-end justify-end h-56 w-full bg-cover">
+                <img src={item.image} alt="pic" />
               </div>
-              ))
-            }
+              <div className="px-5 py-3">
+                <h3 className="text-gray-700 uppercase">{item.title}</h3>
+                <span className="text-gray-500 mt-2">{item.price} Rs</span>
+              </div>
+              <button className="w-1/2 p-2 rounded bg-blue-600 text-white mx-20 mb-4 hover:bg-green-500 focus:outline-none focus:bg-blue-500">
+                See Details
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'man-products': "url('https://images.unsplash.com/photo-1617137968427-85924c800a22?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVuJTIwZmFzaGlvbnxlbnwwfHwwfHx8Mg%3D%3D')",
+        'women-products': "url('https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+      }
+    },
   },
   plugins: [],
 }
