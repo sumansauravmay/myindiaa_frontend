182b637be5eceecdb9e3f0a12dcda02f4460ed47
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-26 20:16:09 +0530

    register

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
-  )
-}
+    <div className="w-full">
+      <h1 className="flex justify-center text-5xl font-bold text-blue-800">Sign Up</h1>
+      <div className="flex justify-center mt-10">
+      <div>
+        <label
+          htmlFor="Email"
+          className="text-md font-medium leading-6 text-gray-900 ml-auto"
+        >
+          Email
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Email"
+          />
+        </div>
+      </div>
 
-export default Register
+      </div>
+      {/* Phone Number */}
+<div className="flex justify-center">
+      <div>
+        <label
+          htmlFor="phone"
+          className="text-md font-medium leading-6 text-gray-900"
+        >
+          Phone
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Phone"
+          />
+        </div>
+      </div>
+      </div>
+      {/* Name */}
+
+<div className="flex justify-center">
+      <div>
+        <label
+          htmlFor="Name"
+          className="text-md font-medium leading-6 text-gray-900"
+        >
+          Name
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Name"
+          />
+        </div>
+      </div>
+      </div>
+
+      {/* Password */}
+
+
+      <div className="flex justify-center">
+      <div>
+        <label
+          htmlFor="Password"
+          className="text-md font-medium leading-6 text-gray-900"
+        >
+          Password
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+      </div>
+
+
+<div className="flex justify-center">
+<button className="p-2 bg-black text-white mt-5 rounded-lg">Sign Up</button>
+</div>
+    </div>
+  );
+};
+
+export default Register;
--- src/Components/WomenProducts/Womenproducts.jsx
+++ src/Components/WomenProducts/Womenproducts.jsx
@@ -5,7 +5,7 @@ import { FaSearch } from "react-icons/fa";
 
 const Womenproducts = () => {
   const [data, setData] = useState([]);
-  const [searchdata, setSearchdata]=useState("");
+  const [searchdata, setSearchdata] = useState("");
 
   const getData = async () => {
     try {
@@ -23,6 +23,7 @@ const Womenproducts = () => {
   useEffect(() => {
     getData();
   }, []);
+  
   return (
     <div className="container mx-auto px-6">
       <div className="flex justify-center">
@@ -36,7 +37,7 @@ const Womenproducts = () => {
             type="text"
             className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-black ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             placeholder="Search"
-            onChange={(e)=>setSearchdata(e.target.value)}
+            onChange={(e) => setSearchdata(e.target.value)}
           />
         </div>
       </div>
@@ -51,17 +52,17 @@ const Womenproducts = () => {
               return item.category == "women";
             })
             .filter((item) => {
-                if (item === "All") {
-                  return item;
-                } else if (
-                  item.title.toLowerCase().includes(searchdata.toLowerCase()) ||
-                  item.description
-                    .toLowerCase()
-                    .includes(searchdata.toLowerCase())
-                ) {
-                  return item;
-                }
-              })
+              if (item === "All") {
+                return item;
+              } else if (
+                item.title.toLowerCase().includes(searchdata.toLowerCase()) ||
+                item.description
+                  .toLowerCase()
+                  .includes(searchdata.toLowerCase())
+              ) {
+                return item;
+              }
+            })
             .map((item) => (
               <ProductCard
                 key={item.id}
