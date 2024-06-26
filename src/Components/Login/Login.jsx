9a6032b3a4ae3467edd1aa25488f0c25fe2f5216
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-26 20:18:34 +0530

    login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="w-full">
-      <h1 className="flex justify-center text-5xl font-bold text-blue-800">Sign Up</h1>
+      <h1 className="flex justify-center text-5xl font-bold text-blue-800">Login</h1>
       <div className="flex justify-center mt-10">
       <div>
         <label
@@ -22,49 +22,8 @@ const Login = () => {
           />
         </div>
       </div>
-
       </div>
-      {/* Phone Number */}
-<div className="flex justify-center">
-      <div>
-        <label
-          htmlFor="phone"
-          className="text-md font-medium leading-6 text-gray-900"
-        >
-          Phone
-        </label>
-        <div className="relative mt-2 rounded-md shadow-sm">
-          <input
-            type="text"
-            name="price"
-            id="price"
-            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Phone"
-          />
-        </div>
-      </div>
-      </div>
-      {/* Name */}
-
-<div className="flex justify-center">
-      <div>
-        <label
-          htmlFor="Name"
-          className="text-md font-medium leading-6 text-gray-900"
-        >
-          Name
-        </label>
-        <div className="relative mt-2 rounded-md shadow-sm">
-          <input
-            type="text"
-            name="price"
-            id="price"
-            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Name"
-          />
-        </div>
-      </div>
-      </div>
+     
 
       {/* Password */}
 
@@ -89,9 +48,8 @@ const Login = () => {
       </div>
       </div>
 
-
 <div className="flex justify-center">
-<button className="p-2 bg-black text-white mt-5 rounded-lg">Sign Up</button>
+<button className="p-2 bg-black text-white mt-5 rounded-lg">Login</button>
 </div>
     </div>
   )
