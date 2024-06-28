0d109951e04d326b2dd1c3ac4220d6497fea9b13
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-29 05:12:27 +0530

    product details

--- src/Components/ProductsDetailsCard.jsx
+++ src/Components/ProductsDetailsCard.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const ProductsDetailsCard = ({image, title, description, category, price}) => {
+const ProductsDetailsCard = ({
+  image,
+  title,
+  description,
+  rating,
+  count,
+  price,
+  cartData,
+}) => {
   return (
     <div className="md:flex mt-8 md:-mx-4">
       <div className="w-full h-92 md:mx-4 rounded-md bg-cover bg-center md:w-1/2 shadow-lg shadow-indigo-500/40">
@@ -14,27 +22,27 @@ const ProductsDetailsCard = ({image, title, description, category, price}) => {
       <div className="w-full h-92 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md:w-1/2 shadow-md shadow-indigo-500/40">
         <div className="bg-opacity-50 flex items-center h-full">
           <div className="px-10 max-w-xl">
-            <h2 className="text-3xl text-black font-semibold">
-              {title}
-            </h2>
-            <p className="mt-2 text-black text-xl">
-              {description}
-            </p>
-            <p className="mt-2 text-black text-lg">
-              Category: {category}
-            </p>
-            {/* <p className="mt-2 text-black">Rating: {productDetails.rating.rate}- {productDetails.rating.count}</p> */}
-            <p className="mt-2 text-black text-2xl">
-              Price: {price}
-            </p>
-            <button className="mt-2 p-1 text-white text-2xl bg-indigo-500 rounded-lg">
+            <h2 className="text-3xl text-black font-semibold">{title}</h2>
+            <p className="mt-2 text-black text-xl">{description}</p>
+
+            <div className="flex space-x-1 text-xl">
+              <h5 className="mt-2 text-black">Rating: </h5>
+              <h5 className="mt-2 text-black font-semibold">{rating}</h5>
+              <p className="mt-2 text-black font-semibold">({count})</p>
+            </div>
+
+            <p className="mt-2 text-black text-2xl">Price: {price} ₹</p>
+            <button
+              className="mt-2 p-1 text-white text-2xl bg-indigo-500 rounded-lg"
+              onClick={cartData}
+            >
               Add to cart
             </button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsDetailsCard
+export default ProductsDetailsCard;
--- src/Pages/ProductsDetails.jsx/ProductsDetails.jsx
+++ src/Pages/ProductsDetails.jsx/ProductsDetails.jsx
@@ -4,7 +4,8 @@ import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { cart } from "../../Redux/CartReducer/action";
 import Loading from "../../Components/Loading";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
+import ProductsDetailsCard from "../../Components/ProductsDetailsCard";
 
 const ProductsDetails = () => {
   const [loading, setLoading] = useState(false);
@@ -33,52 +34,19 @@ const ProductsDetails = () => {
 
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Product Details Page</title>
         <meta name="description" content="Product Details Page description" />
       </Helmet>
-    <div className="md:flex mt-8 md:-mx-4">
-      <div className="w-full h-92 md:mx-4 rounded-md bg-cover bg-center md:w-1/2 shadow-lg shadow-indigo-500/40">
-        <div className="flex items-center h-full">
-          <div className="px-10 max-w-xl">
-            <img className="h-[32rem]" src={productDetails.image} alt="pic" />
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full h-92 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md:w-1/2 shadow-md shadow-indigo-500/40">
-        <div className="bg-opacity-50 flex items-center h-full">
-          <div className="px-10 max-w-xl">
-            <h2 className="text-3xl text-black font-semibold">
-              {productDetails.title}
-            </h2>
-            <p className="mt-2 text-black text-xl">
-              {productDetails.description}
-            </p>
-
-            <div className="flex space-x-1 text-xl">
-              <h5 className="mt-2 text-black">Rating: </h5>
-              <h5 className="mt-2 text-black font-semibold">
-                {productDetails.rating?.rate}
-              </h5>
-              <p className="mt-2 text-black font-semibold">
-                ({productDetails.rating?.count})
-              </p>
-            </div>
-
-            <p className="mt-2 text-black text-2xl">
-              Price: {productDetails.price} ₹
-            </p>
-            <button
-              className="mt-2 p-1 text-white text-2xl bg-indigo-500 rounded-lg"
-              onClick={() => dispatch(cart(productDetails))}
-            >
-              Add to cart
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <ProductsDetailsCard
+        image={productDetails.image}
+        title={productDetails.title}
+        description={productDetails.description}
+        price={productDetails.price}
+        rating={productDetails.rating?.rate}
+        count={productDetails.rating?.count}
+        cartData={() => dispatch(cart(productDetails))}
+      />
     </>
   );
 };
